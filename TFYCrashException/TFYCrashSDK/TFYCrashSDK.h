1b50d96c9e92babc34a3ddb2f87e6d56e038076f
田风有 <420144542@qq.com>
2023-02-24 18:26:20 +0800

    #import "TFYCrashExceptionProxy.h"
    #import "TFYCrashExceptionProxy.h"

---
+++ TFYCrashException/TFYCrashSDK/TFYCrashSDK.h
@@ -0,0 +1,13 @@
+//
+//  TFYCrashSDK.h
+//  TFYCrashException
+//
+//  Created by 田风有 on 2023/2/24.
+//
+
+#ifndef TFYCrashSDK_h
+#define TFYCrashSDK_h
+
+#import "TFYCrashExceptionProxy.h"
+
+#endif /* TFYCrashSDK_h */
